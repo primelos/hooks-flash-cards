61e4516ce7799ed9abf9b785a55edbda62b4a0a6
Ale <alearanibar@gmail.com>
2020-07-06 18:51:23 -0700

    flash-card app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./App.css";
+import QuizQuestionForm from './QuizForm'
+import Quiz from './Quiz'
+import { Route, Redirect, Link } from 'react-router-dom'
+import Carousel from './Carousel'
 
 function App() {
-  return <div className="App" >Quiz App</div>;
-}
+  return (
+    
+  <div className="App" >
+    <header>
+      <Link to='/quiz' className='link'>
+        <h1>Quiz</h1>
+        <Link to='/form'>
+          <h1>Add Questions</h1>
+        </Link>
+      </Link>
+    </header>
+    <Route exact path='/' render={() =><Redirect to='/quiz' />} />
+    <Route exact path='/quiz' component={Quiz}/>
+    <Route exact path='/form' component={QuizQuestionForm} />
 
+  </div>
+  )
+}
+  
 export default App;
--- src/Quiz/index.js
+++ src/Quiz/index.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import isEmpty from "lodash/isEmpty";
+import { Link } from "react-router-dom";
+import Carousel from "../Carousel";
+import { carouselContext } from "../context";
 
 function Quiz() {
+  const [questionsList, setQuestionsList] = useState(getQuestionsList());
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [index, setIndex] = useState(0)
+  function handleFlip() {
+    setIsFlipped(!isFlipped);
+  }
   
+  function getQuestionsList() {
+    const storageQuestionsList = window.localStorage.getItem("questionsList");
+    return storageQuestionsList ? JSON.parse(storageQuestionsList) : [];
+  }
+
+  function handleClickRight(){
+    setIndex((index + 1) % questionsList.length)
+  }
+  function handleClickLeft(){
+    index === 0 ? setIndex(questionsList.length - 1) : setIndex(index - 1)
+  }
+
+  function handleDeleteCard(){
+    const newQuestionsList = questionsList.filter((_,i) => index !== i)
+    if (index >= newQuestionsList.length || index < 0) setIndex(0)
+    setQuestionsList(newQuestionsList)
+    window.localStorage.setItem(
+      'questionsList',
+      JSON.stringify(newQuestionsList)
+    )
+  }
+  
+
+  const carouselContextValue = {
+    handleFlip,
+    isFlipped,
+    questionsList,
+    index,
+    handleClickRight,
+    handleClickLeft
+  };
+
   return (
     <div>
-      FlashCards here
+      {!isEmpty(questionsList) ? (
+        <div>
+          <carouselContext.Provider value={carouselContextValue}>
+            <Carousel />
+          </carouselContext.Provider>
+          <button className='delete-button' onClick={handleDeleteCard}>Delete</button>
+        </div>
+      ) : (
+        <div className='flip-card front'>
+          There are no questions available , please click{" "}
+          <Link to="/form">here</Link> to add some
+        </div>
+      )}
     </div>
   );
 }
